2e317cba379a4046626a1ec9f1652e4e94d7f1e1
MihaiFilimon04 <147743930+MihaiFilimon04@users.noreply.github.com>
2024-11-07 23:47:51 +0200

    Update Movie.java

--- Movie.java
+++ Movie.java
@@ -1,28 +1,33 @@
-class Movie {
-    // class A
-    String title;
-    String genre;
-    boolean isAvailable;
+// Movie.java
+public class Movie implements Comparable<Movie> {
+    private String title;
+    private String genre;
+    private int year;
 
-    // constructor for Movie
+    // Constructor
     public Movie(String title, String genre) {
         this.title = title;
         this.genre = genre;
-        this.isAvailable = true; // movie is available by default
+        this.year = year;
     }
 
-    public String getTitle() {
-        return title;
+    // Getters
+    public String getTitle() { return title; }
+    public String getGenre() { return genre; }
+    public int getYear() { return year; }
+
+    // Implement Comparable to compare Movies by title, then by year if titles are identical
+    @Override
+    public int compareTo(Movie other) {
+        int titleComparison = this.title.compareTo(other.title);
+        if (titleComparison != 0) {
+            return titleComparison;
+        }
+        return Integer.compare(this.year, other.year);
     }
 
-    public String getGenre() {
-        return genre;
-    }
-
-    public boolean isAvailable() {
-        return isAvailable;
-    }
-    public void setAvailable(boolean available) {
-        isAvailable = available;
+    @Override
+    public String toString() {
+        return "Movie{" + "title='" + title + '\'' + ", genre='" + genre + '\'' + ", year=" + year + '}';
     }
 }
